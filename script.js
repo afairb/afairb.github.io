70c998f5de0e5e096b627526a5573966cf4f5cdb
afairb <wave3k@gmail.com>
2014-07-08 17:33:42 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function() {
+    var a = document.getElementsByTagName("a");
+    for (var i = 0, j = a.length; i < j; i++) {
+        a[i].addEventListener("click", function(e) {
+            var url = this.href;
+            ga("send", "event", "outbound", "click", url, {"hitCallback": function () {
+                document.location = url;
+            }});
+            e.preventDefault();
+        });
+    }
+
+    var k,
+        animation = document.getElementById("animation");
+    animation.className = "animate";
+    animation.addEventListener("animationend", function() {
+        animation.className = "";
+        k = 0;
+    });
+    window.addEventListener("mousemove", function() {
+        if (++k == 100) {
+            animation.className = "animate";
+        }
+    });
+});
