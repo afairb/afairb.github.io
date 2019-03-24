80f558454bb4cb988a2e1a460d6a21eecde91ff0
afairb <afairb@users.noreply.github.com>
2019-03-24 19:14:22 +0000

    Refresh content and code

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 window.addEventListener("load", function() {
-    var a = document.getElementsByTagName("a");
-    for (var i = 0, j = a.length; i < j; i++) {
-        a[i].addEventListener("click", function(e) {
-            var url = this.href;
-            ga("send", "event", "outbound", "click", url, {"hitCallback": function() {
-                document.location = url;
-            }});
-            e.preventDefault();
-        });
-    }
-
-    var k,
-        animation = document.getElementById("animation");
-    animation.className = "animate";
-    animation.addEventListener("animationend", function() {
-        animation.className = "";
-        k = 0;
+  var a = document.getElementsByTagName("a");
+  for (var i = 0, j = a.length; i < j; i++) {
+    a[i].addEventListener("click", function() {
+      var url = this.href;
+      ga("send", "event", {
+        eventCategory: "outbound",
+        eventAction: "click",
+        eventLabel: url,
+        transport: "beacon"
+      });
     });
-    window.addEventListener("mousemove", function() {
-        if (++k == 100) {
-            animation.className = "animate";
-        }
-    });
+  }
 });
